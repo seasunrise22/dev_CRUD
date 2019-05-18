d77314e4cd3ec7e13ec6d4dc4016266e45260a7a
seasunrise22 <seasunrise22@gmail.com>
2019-05-18 13:00:51 +0900

    최초 커밋

---
+++ src/com/dev/controller/FrontController.java
@@ -0,0 +1,44 @@
+package com.dev.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	String charset = null;
+	HashMap<String, Controller> list = null;
+
+	/*
+	 * init() : 클라이언트로부터 최초로 서블릿을 호출받았을 때 실행되는 메소드
+	 */
+	@Override
+	public void init(ServletConfig sc) throws ServletException {
+		charset = sc.getInitParameter("charset");
+		list = new HashMap<String, Controller>();
+		list.put("/memberInsert.do", new MemberInsertController());
+		list.put("/memberSearch.do", new MemberSearchController());
+		list.put("/memberUpdate.do", new MemberUpdateController());
+		list.put("/memberDelete.do", new MemberDeleteController());
+		list.put("/memberList.do", new MemberListController());
+	}
+
+	/*
+	 * service() : 클라이언트로부터 요청이 있을 때 마다 실행되는 메소드
+	 */
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding(charset);
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String path = uri.substring(contextPath, length());
+		Controller subController = list.get(path);
+		subController.execute(request, response);		
+	}
+}
